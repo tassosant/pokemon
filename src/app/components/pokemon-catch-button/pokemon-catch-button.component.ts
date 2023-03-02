694f3a4a9a0d9a20f045d598b31de17fc01b7dc8
Tasos Antoniou <tasos.an.antoniou@gmail.com>
2023-03-02 23:43:36 +0200

    bug: not proper checking if the same pokemon exists in trainer's pack

--- src/app/components/pokemon-catch-button/pokemon-catch-button.component.ts
+++ src/app/components/pokemon-catch-button/pokemon-catch-button.component.ts
@@ -1,26 +1,36 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PokemonCaughtService } from 'src/app/services/pokemon-caught.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 @Component({
   selector: 'app-pokemon-catch-button',
   templateUrl: './pokemon-catch-button.component.html',
   styleUrls: ['./pokemon-catch-button.component.css']
 })
-export class PokemonCatchButtonComponent {
+export class PokemonCatchButtonComponent implements OnInit{
 
-  @Input() pokemonName: string="";
+  public isCaught: boolean = false;
+  @Input() pokemonId!: number;
 
   get loading(): boolean{
     return this.pokemonCaughtService.loading;
   }
 
-  constructor(private readonly pokemonCaughtService: PokemonCaughtService){}
+  // get isCaught(): boolean{
+  //   return this.trainerService.inPack(this.pokemonName);
+  // }
+  ngOnInit(): void {
+      this.isCaught = this.trainerService.inPack(this.pokemonId);
+  }
+  constructor(
+    private trainerService: TrainerService,
+    private readonly pokemonCaughtService: PokemonCaughtService){}
 
   onCatchClick(): void{
     //Add the pokemon to trainer's pack
     // alert(this.pokemonName+' was caught!!');
-    this.pokemonCaughtService.addToPack(this.pokemonName)
+    this.pokemonCaughtService.addToPack(this.pokemonId)
     .subscribe({
       next:(response:any)=>{
         console.log("NEXT", response);
